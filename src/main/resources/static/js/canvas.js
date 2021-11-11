1b7abee0c91eef2fc9628d181d25d6d5f28cd159
Nick <nicholas_sawicki@brown.edu>
2021-11-11 17:08:29 -0500

    partially through

--- src/main/resources/static/js/canvas.js
+++ src/main/resources/static/js/canvas.js
@@ -1,6 +1,6 @@
 // TODO: set the number of rows and cols in the toggle pane
-const TOGGLE_ROWS = ?;
-const TOGGLE_COLS = ?;
+const TOGGLE_ROWS = 2;
+const TOGGLE_COLS = 3;
 
 // set the pixel size of each tile
 const TILE_HEIGHT = 50;
@@ -14,7 +14,7 @@ let ctx;
 
 // TODO: create an array of strings with the 2 different
 // toggle settings (prefix and whitespace)
-let flags = ?;
+let flags = ["settings", "whitespace"];
 
 // array used to store currently selected toggle settings
 let selection = [];
@@ -30,16 +30,18 @@ $(document).ready(() => {
   // TODO: set the width and height of canvas
   // hint -- use the globally defined TOGGLE_ROWS, TOGGLE_COLS,
   //         TILE_HEIGHT, TILE_WIDTH
-  canvas.width = ?;
-  canvas.height = ?;
+  canvas.width = TOGGLE_COLS*TILE_WIDTH;
+  canvas.height = TOGGLE_ROWS*TILE_HEIGHT;
 
   // TODO: set up the canvas context
+  ctx = canvas.getContext('2d'); // FIGURE OUT WHAT THE CONTEXT ID IS
 
   // paints the toggle pane
   paintToggle();
 
   // TODO: add a click handler for when the user clicks the canvas element
   // with id 'toggle'. Have it call the function 'paintOnClick'.
+  canvas.addEventListener("click", paintOnClick());
 
 
   // click handler for the submit button
@@ -93,12 +95,28 @@ const paintToggle = () => {
 
   // TODO: fill the background color of the canvas element to
   // something other than white using ctx.fillStyle() and ctx.fillRect()
+  ctx.fillStyle = 'green'
+  ctx.fillRect(0,0,TILE_WIDTH,TILE_HEIGHT);
 
 
   // TODO: draw the grid lines for the toggle pane using ctx.beginPath()
   // and ctx.moveTo()
   // hint -- remember you have the fields TOGGLE_ROWS & TOGGLE_COLS, and
   //         TILE_HEIGHT & TILE_WIDTH, which will help you draw the lines
+  ctx.beginPath();
+  ctx.strokeStyle = 'black';
+  // TAKES CARE OF VERTICAL LINES
+  for(let i=0; i<TOGGLE_COLS; i++) {
+    ctx.moveTo(i*TILE_WIDTH,0);
+    ctx.lineTo(i*TILE_WIDTH,TILE_HEIGHT*TOGGLE_ROWS);
+  }
+  // TAKES CARE OF HORIZONTAL LINES
+  for(let i=0; i<TOGGLE_ROWS; i++) {
+    ctx.moveTo(0,i*TILE_HEIGHT);
+    ctx.lineTo(TILE_WIDTH*TOGGLE_COLS,i*TILE_HEIGHT);
+  }
+  ctx.stroke();
+
 
 
   // populate the toggle pane's text using ctx.fillText(). Reference
